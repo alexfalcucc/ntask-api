966531383b58011d5c22b2c72d5e53d626a77fd1
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-03-13 10:17:30 -0300

    added models for tasks

--- models/tasks.js
+++ models/tasks.js
@@ -1,10 +1,30 @@
-module.exports = app => {
-    return {
-        findAll: (params, callback) => {
-            return callback([
-                {title: "Go Shopping"},
-                {title: "Fix notebook"},
-            ]);
+module.exports = (sequelize, DataType) => {
+    const Tasks = sequelize.define("Tasks", {
+        id: {
+            type: DataType.INTEGER, 
+            primaryKey: true,
+            autoIncrement: true
+        },  
+        title: {
+            type: DataType.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true 
+            }
+        },
+        done: {
+            type: DataType.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
         }
-    };
+    }, {
+        classMethods: {
+            associate: (models) => {
+                Tasks.belongsTo(models.Users);
+                }
+            }  
+        }
+    });
+    return Tasks;
 };
+
