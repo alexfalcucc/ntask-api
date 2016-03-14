e6f24fa6b39e1397e4f40ae9527061231732b966
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-03-13 23:43:35 -0300

    midleware refactor

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,6 @@
 module.exports = app => {
      const Tasks = app.db.models.Tasks;
      app.route("/tasks")
-        .all((req, res) => {
-            // Routes Middleware of pre-run 
-            delete req.body.id;
-            next();
-        })
         .get((req, res) => {
             // "/tasks": task's list 
             Tasks.findAll({})
@@ -23,11 +18,6 @@ module.exports = app => {
                 });
         });
     app.route("/tasks/:id")
-        .all((req, res, next) => {
-            // same porpuse.
-            delete req.body.id;
-            next();
-        })
         .get((req, res) => {
             Tasks.findOne({where: req.params})
                 .then(result => {
