17eed12a1cb2d96c3c2dfbb11e33af8843e28243
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-03-13 23:11:01 -0300

    added methods on routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,27 @@
 module.exports = app => {
      const Tasks = app.db.models.Tasks;
-     app.get("/tasks", (req, res) => {
-            Tasks.findAll({}).then(tasks => {
-                res.json({tasks: tasks});
+     app.route("/tasks")
+        .all((req, res) => {
+        // Routes Middleware of pre-run 
+            delete req.body.id;
+            next();
+        })
+        .get((req, res) => {
+        // "/tasks": task's list 
+        })
+        .post((req, res) => {
+        // "/tasks": Insert a new task 
+        })
+    app.route("/tasks/:id")
+        .all((req, res) => {
+        // same porpuse.
+            delete req.body.id;
+            next();
+        })
+        .get((req, res) => {
+        })
+        .put((req, res) => {
+        })
+        .delete((req, res) => {
         });
-     });
 };
