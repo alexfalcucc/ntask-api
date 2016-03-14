df01f81d443e166f387432fcc68d949b6cf8d769
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-03-13 23:37:19 -0300

    added get method

--- routes/tasks.js
+++ routes/tasks.js
@@ -29,8 +29,24 @@ module.exports = app => {
             next();
         })
         .get((req, res) => {
+            Tasks.findOne({where: req.params})
+                .then(result => {
+                    if (result) {
+                        res.json(result);
+                    } else {
+                        res.sendStatus(404);
+                    }
+                })
+                .catch(error => {
+                    res.status(412).json({msg: error.message});
+                });
         })
         .put((req, res) => {
+            Tasks.update(req.body, {where: req.params})
+                .then(result => res.sendStatus(204))
+                .catch(error => {
+                    res.status(412).json({msg: error.message});
+                });
         })
         .delete((req, res) => {
         });
